1fb23a3f4131a01a3296af4d79a31e0bdc99540c
ssola <introduccio@gmail.com>
2016-01-26 01:15:48 +0100

    code reformatted

--- resizer.go
+++ resizer.go
@@ -1,64 +1,64 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"io"
-	"github.com/nfnt/resize"
-	"image"
-	"image/jpeg"
-	"image/png"
-	"strconv"
+    "fmt"
+    "net/http"
+    "io"
+    "github.com/nfnt/resize"
+    "image"
+    "image/jpeg"
+    "image/png"
+    "strconv"
 )
 
 // Return a given error in JSON format to the ResponseWriter
 func format_error(err error, w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json")
-	io.WriteString(w, fmt.Sprintf("{ \"error\": \"%s\"}", err))
-	return
+    w.Header().Set("Content-Type", "application/json")
+    io.WriteString(w, fmt.Sprintf("{ \"error\": \"%s\"}", err))
+    return
 }
 
 // Parse a given string into a uint value
 func parseInteger(value string) (uint, error) {
-	integer, err := strconv.Atoi(value)
-	return uint(integer), err
+    integer, err := strconv.Atoi(value)
+    return uint(integer), err
 }
 
 // Resizing endpoint.
 func resizing(w http.ResponseWriter, r *http.Request) {
-	var newWidth, newHeight uint
+    var newWidth, newHeight uint
 
-	// Get parameters
-	originalImage := r.FormValue("image")
-	newWidth, _ = parseInteger(r.FormValue("width"))
-	newHeight, _ = parseInteger(r.FormValue("height"))
+    // Get parameters
+    originalImage := r.FormValue("image")
+    newWidth, _ = parseInteger(r.FormValue("width"))
+    newHeight, _ = parseInteger(r.FormValue("height"))
 
-	// Download the image
-	oImage, err := http.Get(originalImage)
-	if err != nil {
-		format_error(err, w)
-	}
+    // Download the image
+    oImage, err := http.Get(originalImage)
+    if err != nil {
+        format_error(err, w)
+    }
 
-	finalImage, _, _ := image.Decode(oImage.Body)
+    finalImage, _, _ := image.Decode(oImage.Body)
 
-	r.Body.Close()
+    r.Body.Close()
 
-	imageInMemory := resize.Resize(newWidth, newHeight, finalImage, resize.Lanczos3)
+    imageInMemory := resize.Resize(newWidth, newHeight, finalImage, resize.Lanczos3)
 
-	if oImage.Header.Get("Content-Type") == "image/png" {
-		png.Encode(w, imageInMemory)
-	}
+    if oImage.Header.Get("Content-Type") == "image/png" {
+        png.Encode(w, imageInMemory)
+    }
 
-	if oImage.Header.Get("Content-Type") == "image/jpg" {
-		jpeg.Encode(w, imageInMemory, nil)
-	}
+    if oImage.Header.Get("Content-Type") == "image/jpg" {
+        jpeg.Encode(w, imageInMemory, nil)
+    }
 
-	if oImage.Header.Get("Content-Type") == "binary/octet-stream" {
-		jpeg.Encode(w, imageInMemory, nil)
-	}
+    if oImage.Header.Get("Content-Type") == "binary/octet-stream" {
+        jpeg.Encode(w, imageInMemory, nil)
+    }
 }
 
 func main() {
-	http.HandleFunc("/resize", resizing)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/resize", resizing)
+    http.ListenAndServe(":8080", nil)
 }
