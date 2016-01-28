03e72318963b1e574145e9b992eb816cb0e91d66
ssola <introduccio@gmail.com>
2016-01-28 20:45:42 +0100

    added configuration file with viper

--- resizer.go
+++ resizer.go
@@ -8,20 +8,26 @@ import (
     "image/jpeg"
     "image/png"
     "strconv"
+    "github.com/spf13/viper"
 )
+
+type Configuration struct {
+    Size Size
+}
 
 type Size struct {
     Width uint
     Height uint
 }
 
-func validateSize(s *Size) error {
-    if s.Height >= 1000 {
-        return error(fmt.Errorf("Height cannot be higher than 1000"))
+// Including basic validation to prevent creating big images
+func validateSize(c *Configuration, s *Size) error {
+    if  s.Height >= c.Size.Height {
+        return error(fmt.Errorf("Height cannot be higher than %d", c.Size.Height))
     }
 
-    if s.Width >= 1000 {
-        return error(fmt.Errorf("Width cannot be higher than 1000"))
+    if s.Width >= c.Size.Width {
+        return error(fmt.Errorf("Width cannot be higher than %d", c.Size.Width))
     }
 
     return nil
@@ -40,14 +46,25 @@ func parseInteger(value string) (uint, error) {
 
 // Resizing endpoint.
 func resizing(w http.ResponseWriter, r *http.Request) {
-    size := Size{}
+    // Load configuration
+    viper.SetConfigName("config")
+    viper.AddConfigPath(".")
+    if err := viper.ReadInConfig(); err != nil {
+        panic(fmt.Errorf("Fatal error loading configuration file: %s", err))
+    }
+
+    config := new(Configuration)
+    size := new(Size)
+
+    // Marshal the configuration into our Struct
+    viper.Unmarshal(&config)
 
     // Get parameters
     imageUrl := r.FormValue("image")
     size.Width, _ = parseInteger(r.FormValue("width"))
     size.Height, _ = parseInteger(r.FormValue("height"))
 
-    if err := validateSize(&size); err != nil {
+    if err := validateSize(config, size); err != nil {
         formatError(err, w)
         return
     }
@@ -60,7 +77,6 @@ func resizing(w http.ResponseWriter, r *http.Request) {
     }
 
     finalImage, _, _ := image.Decode(imageBuffer.Body)
-
     r.Body.Close()
 
     imageResized := resize.Resize(size.Width, size.Height, finalImage, resize.Lanczos3)
