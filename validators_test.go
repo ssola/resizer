bdda1d9d31ccc1bf11e6f372a2333e9675be86ee
ssola <introduccio@gmail.com>
2016-01-28 23:07:06 +0100

    more tests and add travis

--- validators_test.go
+++ validators_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+    "testing"
+)
 
 func TestCheckHostInWhiteListWithEmptyConfiguration(t *testing.T) {
     config := new(Configuration)
@@ -8,5 +10,17 @@ func TestCheckHostInWhiteListWithEmptyConfiguration(t *testing.T) {
 
     if err := validator.CheckHostInWhiteList("doesnt exists"); err == nil {
         t.Errorf("Missing error returning!")
+    }
+}
+
+func TestCheckHostInWhiteListWithSomeHostsInWhieList(t *testing.T) {
+    config := new(Configuration)
+    config.DomainWhiteList = []string{"one host", "two hosts"}
+    validator := Validator{config}
+
+    // Check for one that doesn't exists
+    err := validator.CheckHostInWhiteList("one host2")
+    if err.Error() != "Host  not allowed" {
+        t.Errorf("Should return an error!!!")
     }
 }
