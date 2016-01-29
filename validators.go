ca21805f839fc090cca054e8e284ed692bd37fc0
Sergio Sola <introduccio@gmail.com>
2016-01-29 16:05:04 +0100

    format code

--- validators.go
+++ validators.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-    "net/url"
-    "fmt"
-    "regexp"
+	"fmt"
+	"net/url"
+	"regexp"
 )
 
 type Validator struct {
-    config *Configuration
+	config *Configuration
 }
 
 // Checks if given request image host belongs to one in the
 // white list.
 func (v *Validator) CheckHostInWhiteList(requestUrl string) error {
-    urlParsed, err := url.Parse(requestUrl)
+	urlParsed, err := url.Parse(requestUrl)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    var hostFound bool
+	var hostFound bool
 
-    for _, host := range v.config.HostWhiteList {
-        match, _ := regexp.MatchString(host, requestUrl)
-        if match {
-            hostFound = true
-        }
-    }
+	for _, host := range v.config.HostWhiteList {
+		match, _ := regexp.MatchString(host, requestUrl)
+		if match {
+			hostFound = true
+		}
+	}
 
-    if hostFound {
-        return nil
-    }
+	if hostFound {
+		return nil
+	}
 
-    return error(fmt.Errorf("Host %s not allowed", urlParsed.Host))
+	return error(fmt.Errorf("Host %s not allowed", urlParsed.Host))
 }
 
 // Validates if new request size is valid or not
 func (v *Validator) CheckRequestNewSize(s *Size) error {
-    if s.Height <= 0 || s.Width <= 0 {
-        return error(fmt.Errorf("Width or height should be bigger than 0"))
-    }
+	if s.Height <= 0 || s.Width <= 0 {
+		return error(fmt.Errorf("Width or height should be bigger than 0"))
+	}
 
-    if  s.Height >= v.config.Size.Height {
-        return error(fmt.Errorf("Height cannot be higher than %d", v.config.Size.Height))
-    }
+	if s.Height >= v.config.Size.Height {
+		return error(fmt.Errorf("Height cannot be higher than %d", v.config.Size.Height))
+	}
 
-    if s.Width >= v.config.Size.Width {
-        return error(fmt.Errorf("Width cannot be higher than %d", v.config.Size.Width))
-    }
+	if s.Width >= v.config.Size.Width {
+		return error(fmt.Errorf("Width cannot be higher than %d", v.config.Size.Width))
+	}
 
-    return nil
+	return nil
 }
--- validators_test.go
+++ validators_test.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestCheckHostInWhiteListWithEmptyConfiguration(t *testing.T) {
-    config := new(Configuration)
-    validator := Validator{config}
+	config := new(Configuration)
+	validator := Validator{config}
 
-    if err := validator.CheckHostInWhiteList("doesnt exists"); err == nil {
-        t.Errorf("Missing error returning!")
-    }
+	if err := validator.CheckHostInWhiteList("doesnt exists"); err == nil {
+		t.Errorf("Missing error returning!")
+	}
 }
 
 func TestCheckHostInWhiteListWithSomeHostsInWhieList(t *testing.T) {
-    config := new(Configuration)
-    config.HostWhiteList = []string{"http://www.google.com", "two hosts"}
-    validator := Validator{config}
+	config := new(Configuration)
+	config.HostWhiteList = []string{"http://www.google.com", "two hosts"}
+	validator := Validator{config}
 
-    // Check for one that doesn't exists
-    err := validator.CheckHostInWhiteList("http://www.sergiosola.com/")
-    if err == nil {
-        t.Errorf("Should return an error!!!")
-    }
+	// Check for one that doesn't exists
+	err := validator.CheckHostInWhiteList("http://www.sergiosola.com/")
+	if err == nil {
+		t.Errorf("Should return an error!!!")
+	}
 }
 
 func TestCheckHostInWhiteListWithValidHost(t *testing.T) {
-    config := new(Configuration)
-    config.HostWhiteList = []string{"one host", "sergiosola.com"}
-    validator := Validator{config}
+	config := new(Configuration)
+	config.HostWhiteList = []string{"one host", "sergiosola.com"}
+	validator := Validator{config}
 
-    err := validator.CheckHostInWhiteList("https://sergiosola.com/images?withParams=dsada")
-    if err != nil {
-        t.Errorf("Should not to return an error!!!")
-    }
+	err := validator.CheckHostInWhiteList("https://sergiosola.com/images?withParams=dsada")
+	if err != nil {
+		t.Errorf("Should not to return an error!!!")
+	}
 }
 
 func TestCheckHostInWhiteListWithValidPattern(t *testing.T) {
-    config := new(Configuration)
-    config.HostWhiteList = []string{"www.google.com", "([a-z]+).cdn.google.com"}
-    validator := Validator{config}
+	config := new(Configuration)
+	config.HostWhiteList = []string{"www.google.com", "([a-z]+).cdn.google.com"}
+	validator := Validator{config}
 
-    err := validator.CheckHostInWhiteList("https://dsadsaasds.cdn.google.com/images?withParams=dsada")
-    if err != nil {
-        t.Errorf("Should not to return an error!!!")
-    }
+	err := validator.CheckHostInWhiteList("https://dsadsaasds.cdn.google.com/images?withParams=dsada")
+	if err != nil {
+		t.Errorf("Should not to return an error!!!")
+	}
 }
